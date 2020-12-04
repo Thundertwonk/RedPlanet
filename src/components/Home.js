6ade5e8f36ad30368baac36a36bc5abff4902e55
dharmitviradia <dharmit.viradia@gmail.com>
2020-12-04 15:17:01 -0500

    news

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import '../App.css';
 import { Grid, makeStyles, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
-    
+
 })
 
 const Home = () =>{
     const classes = useStyles();
     return(
         <div className={classes}>
-        <img src="https://mars.nasa.gov/images/mep/allaboutmars/quickfacts/Mars_QuickFacts_sample7_recolored.png" alt="Girl in a jacket"></img>
+        <img src="https://mars.nasa.gov/images/mep/allaboutmars/quickfacts/Mars_QuickFacts_sample7_recolored.png" alt="mars fact" width="100%"></img>
         </div>
     )
 
